1ca106f2f10cb1fbc129a1fe6d27804c0a9cf208
김정윤 <kimjeongyun@gimjeong-yun-ui-MacBookAir.local>
2021-03-20 22:33:00 +0900

    test

--- APIserver/routes/index.js
+++ APIserver/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET users listing. */
+router.get('/test', function (req, res, next) {
+  res.send('respond with a resource');
+  console.log('연결됨');
 });
 
 module.exports = router;
