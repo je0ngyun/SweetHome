fd58ab7b10ea6cecd978b5761cf7e009af355ef1
김정윤 <kimjeongyun@gimjeong-yun-ui-MacBookAir.local>
2021-03-26 11:24:59 +0900

    db_conn.js 주석추가

--- APIserver/common_modules/db_conn.js
+++ APIserver/common_modules/db_conn.js
@@ -1,6 +1,14 @@
 'use strict';
 const env = require('./env/db_env.json');
-
+//common_modules/env/db_env.json 필요
+/*e.g {
+  "db_env": {
+    "SERVER_PORT": 3000,
+    "DB_HOST": "localhost",
+    "DB_USER": "root",
+    "DB_PASSWORD": "password"
+  }
+}*/
 function db_conn() {
   return env.db_env.DB_HOST; //형식 접근
 }
