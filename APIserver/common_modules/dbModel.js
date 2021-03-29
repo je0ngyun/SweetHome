ccf4559b27f2caa84a66ec20e030e4b5267af4e5
김정윤 <kimjeongyun@gimjeong-yun-ui-MacBookAir.local>
2021-03-29 22:15:20 +0900

    백업

--- APIserver/common_modules/dbModel.js
+++ APIserver/common_modules/dbModel.js
@@ -3,11 +3,44 @@
 const db = require('../common_modules/dbConn');
 const self = {};
 
-self.signup = function (userinfo) {};
+self.signup = async function (info) {
+  try {
+    let ret = await db('user')
+      .insert({
+        id: info.userid,
+        username: info.username,
+        password: info.password, //추후 암호화
+        mail: info.mail,
+        phone_os: info.phone_os,
+        phone_model: info.phone_model,
+      })
+      .then();
+    return true;
+  } catch (ex) {
+    console.log(ex);
+    return false;
+  }
+};
 
-self.getinfo = async function (userid) {
-  let ret = await db('user').where('id', userid).first().then();
-  return ret;
+self.signin = async function (info) {
+  try {
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+self.userinfo = async function (info) {
+  try {
+    let ret = await db('user')
+      .select('*')
+      .where('id', info.userid)
+      .first()
+      .then();
+    return ret;
+  } catch (ex) {
+    console.log(ex);
+    return false;
+  }
 };
 
 module.exports = self;
--- APIserver/routes/user.js
+++ APIserver/routes/user.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 var db = require('../common_modules/dbModel');
 
-router.post('/', function (req, res, next) {
-  db.signup();
+//회원가입
+router.post('/signup', async function (req, res, next) {
+  let result = await db.signup(req.body);
+  res.json(result);
 });
 
-router.get('/', async function (req, res, next) {
-  res.json(await db.getinfo(1));
+//로그인
+router.get('/signin', async function (req, res, next) {
+  let result = await db.signin(req.body);
+});
+
+//회원정보조회
+router.get('/info', async function (req, res, next) {
+  res.json(await db.userinfo(req.body));
 });
 
 module.exports = router;
