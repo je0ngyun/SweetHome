63f8892051fc27edaecface1add2e3f2bcf81737
cwooder <yeong@choejaeyeong-ui-MacBookPro.local>
2021-04-06 19:58:59 +0900

    tmp

--- Arduino/sketch_mar21a/sketch_mar21a.ino
+++ Arduino/sketch_mar21a/sketch_mar21a.ino
@@ -1,16 +1,28 @@
+#include <IRremote.h>
+
 int relayPin = 3;
+int IRpin = 4;
 char on = '0';
+
+IRrecv irrecv(IRpin);
+decode_results results;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
+  irrecv.enableIRIn();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()) {
     on = Serial.read();
+  }
+  if (irrecv.decode()) {
+    Serial.println(results.value);
+
+    irrecv.resume();
   }
   if (on == '1') {
     digitalWrite(relayPin, HIGH);
